a776e373d14867cfcb108779c965b141983e141a
Yishak-KD <yisak04@gmail.com>
2021-08-25 02:30:03 -0700

    Header file

--- shell.h
+++ shell.h
@@ -32,6 +32,30 @@ typedef struct variables
 	char **commands;
 } vars_t;
 
+typedef struct data
+{
+	char **av;
+	char *input;
+	char **args;
+	int status;
+	int counter;
+	char **_environ;
+	char *pid;
+} data_shell;
+
+/**
+ * struct args - arguments
+ * @argc: numbers of args
+ * @argv: double pointer of strings
+ *
+ * Description: struct arguments
+ */
+typedef struct args
+{
+	int argc;
+	char **argv;
+} args_t;
+
 /**
  * struct builtins - struct for the builtin functions
  * @name: name of builtin command
@@ -42,6 +66,34 @@ typedef struct builtins
 	char *name;
 	void (*f)(vars_t *);
 } builtins_t;
+
+/**
+ * struct sep_list_s - single linked list
+ * @separator: ; | &
+ * @next: next node
+ * Description: single linked list to store separators
+ */
+typedef struct sep_list_s
+{
+	char separator;
+	struct sep_list_s *next;
+} sep_list;
+
+/**
+ * struct alias_s - A new struct defining aliases.
+ * @name: The name of the alias.
+ * @value: The value of the alias.
+ * @next: A pointer to another struct alias_s.
+ */
+typedef struct alias_s
+{
+	char *name;
+	char *value;
+	struct alias_s *next;
+} alias_t;
+
+/* Global aliases linked list */
+alias_t *aliases;
 
 char **make_env(char **env);
 void free_env(char **env);
@@ -62,6 +114,7 @@ void _env(vars_t *vars);
 void new_setenv(vars_t *vars);
 void new_unsetenv(vars_t *vars);
 
+int built_help(__attribute__((unused)) char **args);
 void add_key(vars_t *vars);
 char **find_key(char **env, char *key);
 char *add_value(char *key, char *value);
@@ -76,5 +129,38 @@ int check_for_dir(char *str);
 void print_error(vars_t *vars, char *msg);
 void _puts2(char *str);
 char *_uitoa(unsigned int count);
+
+int history(char *input);
+int history_dis(__attribute__((unused))char **c, __attribute__((unused))int s);
+
+void help_all(void);
+void help_alias(void);
+void help_cd(void);
+void help_exit(void);
+void help_help(void);
+void help_env(void);
+void help_setenv(void);
+void help_unsetenv(void);
+void help_history(void);
+
+void assign_lineptr(char **lineptr, size_t *n, char *buffer, size_t b);
+ssize_t _getline(char **lineptr, size_t *n, FILE *stream);
+
+char *readline(void);
+args_t splitline(char *buf);
+int shellby_alias(char **args, char __attribute__((__unused__)) **front);
+void set_alias(char *var_name, char *value);
+void print_alias(alias_t *alias);
+char **replace_aliases(char **args);
+
+sep_list *add_sep_node_end(sep_list **head, char sep);
+
+void cd_dot(data_shell *datash);
+void cd_to(data_shell *datash);
+void cd_previous(data_shell *datash);
+void cd_to_home(data_shell *datash);
+
+int cd_shell(data_shell *datash);
+
 
 #endif /* _SHELL_H_ */
